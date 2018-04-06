c375eaa7854c7aca33c7b7a063e92c00d8b3ea99
hoaiphuong2527 <hoaiphuong2527@gmail.com>
2018-04-06 21:28:48 +0700

    layout part 2

--- app/src/main/java/com/nhom3/project/EasyActivity.java
+++ app/src/main/java/com/nhom3/project/EasyActivity.java
@@ -4,15 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class Introduce4Activity extends AppCompatActivity {
+public class EasyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.introduce4_layout);
-
-        WebView webView = (WebView) findViewById(R.id.wbviewIntro4);
+        setContentView(R.layout.easy_layout);
+        WebView webView = (WebView) findViewById(R.id.ques);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/introduce4.html");
+        webView.loadUrl("file:///android_asset/webview2.html");
     }
 }
