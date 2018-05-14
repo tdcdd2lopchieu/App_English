5f1099bac4e8451827a6c0167dc87999301d3c76
anhtan3396 <anhtan3396@gmail.com>
2018-05-14 09:04:15 +0700

    Revert "Update"
    This reverts commit c1d8edc6a06cefe7f468792cb05385be9fa1ee1e.

--- app/src/main/java/com/nhom3/project/LoginActivity.java
+++ app/src/main/java/com/nhom3/project/LoginActivity.java
@@ -3,11 +3,11 @@ package com.nhom3.project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Introduce3Activity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.introduce3_layout);
+        setContentView(R.layout.login_layout);
     }
 }
