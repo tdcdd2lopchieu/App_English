116b2796c1ba4948007ae9382ba48f5d97808fe4
hoaiphuong2527 <hoaiphuong2527@gmail.com>
2018-05-15 22:39:49 -0700

    fix bug

--- app/src/main/java/com/nhom3/project/EasyActivity.java
+++ app/src/main/java/com/nhom3/project/EasyActivity.java
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 package com.nhom3.project;
 
 import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
-=======
-package com.example.tonan.didong;
-
-import android.app.Dialog;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -34,7 +26,6 @@ public class EasyActivity extends AppCompatActivity {
         pre = (ImageView) this.findViewById(R.id.btnPre);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/webview2.html");
-<<<<<<< HEAD
         webView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -44,26 +35,18 @@ public class EasyActivity extends AppCompatActivity {
 
                 Button btnDone = (Button) dialog.findViewById(R.id.btnDone);
                 btnDone.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    dialog.dismiss();
-                }
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
                 });
-                    dialog.show();
+                dialog.show();
                 return false;
-                }
-=======
-        webView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showdialog();
             }
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
         });
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
                 final Dialog dialog = new Dialog(EasyActivity.this,R.style.Dialog);
                 dialog.setTitle(getResources().getString(R.string.dialog_confirm));
                 dialog.setContentView(R.layout.dialog_confirm_layout);
@@ -80,9 +63,6 @@ public class EasyActivity extends AppCompatActivity {
 
                 dialog.show();
 
-=======
-                showdialog();
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
             }
         });
         nex.setOnClickListener(new View.OnClickListener() {
@@ -103,27 +83,15 @@ public class EasyActivity extends AppCompatActivity {
     {
         if (kt==1)
         {
-<<<<<<< HEAD
             webView.loadUrl("file:///android_asset/webview2.html");
-=======
-            webView.loadUrl("file:///android_asset/webview1.html");
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
             kt=0;
         }
         else
         {
-<<<<<<< HEAD
             webView.loadUrl("file:///android_asset/webview4.html");
             kt=1;
         }
     }
-
-=======
-            webView.loadUrl("file:///android_asset/webview2.html");
-            kt=1;
-        }
-    }
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
     private void showdialog()
     {
         ans = new Dialog(EasyActivity.this);
@@ -135,8 +103,4 @@ public class EasyActivity extends AppCompatActivity {
     {
         ans.hide();
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
 }
--- app/src/main/java/com/nhom3/project/Introduce2Activity.java
+++ app/src/main/java/com/nhom3/project/Introduce2Activity.java
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
+
 package com.nhom3.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class Introduce2Activity extends AppCompatActivity {
-
-=======
-package com.example.tonan.didong;
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
 
 public class Introduce2Activity extends AppCompatActivity {
->>>>>>> 51b60b843f4f4290a4b1ffd6834cf1d522bfc208
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
