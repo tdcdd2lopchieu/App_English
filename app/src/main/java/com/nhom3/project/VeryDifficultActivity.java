f5e63355ae381d64314a52477ff8219551f549fe
hoaiphuong2527 <hoaiphuong2527@gmail.com>
2018-05-16 01:26:35 -0700

    update code ver 1

--- app/src/main/java/com/nhom3/project/VeryDifficultActivity.java
+++ app/src/main/java/com/nhom3/project/VeryDifficultActivity.java
@@ -1,12 +1,15 @@
 package com.nhom3.project;
 
 import android.app.Dialog;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class VeryDifficultActivity extends AppCompatActivity {
@@ -16,13 +19,26 @@ public class VeryDifficultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.very_difficult_layout);
         WebView webView = (WebView) findViewById(R.id.ques);
+        ImageView imgSound = (ImageView) findViewById(R.id.sound);
+        ImageView btnPrev = (ImageView) findViewById(R.id.btnPre);
+        ImageView btnNext = (ImageView) findViewById(R.id.btnNext);
+        Button btnFinish = (Button) findViewById(R.id.submit);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/webview4.html");
+
+        imgSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(VeryDifficultActivity.this,R.raw.audio);
+                mediaPlayer.start();
+            }
+        });
+
         webView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                final Dialog dialog = new Dialog(VeryDifficultActivity.this);
-                dialog.setTitle(R.string.dialog_name);
+                final Dialog dialog = new Dialog(VeryDifficultActivity.this,R.style.Dialog);
+                dialog.setTitle(getResources().getString(R.string.dialog_name));
                 dialog.setContentView(R.layout.dialog_answer_layout);
 
                 Button btnDone = (Button) dialog.findViewById(R.id.btnDone);
@@ -34,6 +50,43 @@ public class VeryDifficultActivity extends AppCompatActivity {
                 });
                 dialog.show();
                 return false;
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(VeryDifficultActivity.this,"Next question",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(VeryDifficultActivity.this,"Previous question",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(VeryDifficultActivity.this,R.style.Dialog);
+                dialog.setTitle(getResources().getString(R.string.dialog_confirm));
+                dialog.setContentView(R.layout.dialog_confirm_layout);
+
+                Button btnYes = (Button) dialog.findViewById(R.id.btnYes);
+                Button btnNo = (Button) dialog.findViewById(R.id.btnNo);
+
+                btnNo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
             }
         });
     }
